b6c3c612b738f1be84be789219ce38355ebe9b88
Zachary Breit <zbreit18@gmail.com>
2019-01-25 14:28:48 -0500

    Added onScroll navigation effects. Removed sky-separators

--- js/main.js
+++ js/main.js
@@ -6,3 +6,18 @@ $("#menu-btn").click(function(e) {
     $("#mobile-top").addClass("hidden");
   }
 });
+
+// Make nav fixed to the top after leaving the intro section
+$(window).scroll(function() {
+  if($(this).scrollTop() > 528) {
+    $('body').addClass('fixed-nav');
+  } else {
+    $('body').removeClass('fixed-nav no-animation');
+  }
+});
+
+$('#nav .nav-link').click(function() {
+  $('body').addClass('no-animation');
+});
+
+
